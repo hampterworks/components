d9bff8868164dae7c82e03e3279a5d24fd9494a5
ohadn <ohad111@gmail.com>
2024-06-19 18:25:03 +0200

    Add 'Typography' and site description to page.tsx
    The `Typography` component from the @repo/ui library is imported to page.tsx, and a site description has been added to the main page to explain the offerings of Hampter Components. The description covers the functionality, flexibility, and utility of the components provided and concludes with an encouraging note.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,17 @@
-import styles from "./page.module.css";
+"use client";
 import React from "react";
-
+import Typography from "@repo/ui/Typography";
+import styles from "./page.module.css";
 
 const Page: React.FC = () => {
   return <main className={styles.main}>
+    <Typography variant='h1' component='h1'>Hampter Components</Typography>
+    <section>
+      Welcome to Hampter Components! We provide a robust collection of Next.js components designed to make your web development process smoother and more efficient. Our library includes a variety of components, each with customizable properties to fit your specific needs.
 
-    </main>
+      Each component is meticulously crafted with flexibility and ease-of-use in mind. Whether you’re building a complex form or a simple user interface, Hampter Components has got you covered. Explore our components and let them empower your Next.js projects! Happy coding! 🚀
+    </section>
+  </main>
 }
 
 export default Page
