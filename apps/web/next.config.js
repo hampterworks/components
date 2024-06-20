ca5ba89925fe8ccd701cead5a56608860dab2689
ohadn <ohad111@gmail.com>
2024-06-20 20:53:37 +0200

    Update next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
-  basePath: '/showcase',
+  basePath: '/components',
   transpilePackages: ["@repo/ui"],
   compiler: {
     styledComponents: true,
