d113e5f2d3471386e5feab153cb3d43523acc4ee
ohadn <ohad111@gmail.com>
2024-06-20 20:22:56 +0200

    Update next.config and implement CI workflow
    Added 'output' and 'basePath' configurations to next.config.js. Also, a new GitHub Actions workflow for continuous integration has been implemented: it triggers on push and pull requests to main branch, includes build and test jobs, and deploys to GitHub Pages.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: "export",
+  basePath: '/showcase',
   transpilePackages: ["@repo/ui"],
   compiler: {
     styledComponents: true,
